40d838aa48b0d7435fb662d6c62088d56c85396b
mrLuisFer <mrluisfeer@gmail.com>
2021-06-12 14:23:52 -0500

    feat: some styles and endpoints rendered

--- src/routes/callbacks/route-root.js
+++ src/routes/callbacks/route-root.js
@@ -1,7 +1,11 @@
 const pathJoin = require('../../utils/pathJoin')
+const { routesTemplate } = require('../routes')
 
 const routeRoot = (request, response) => {
-  response.render(pathJoin('./views/index.ejs'), { title: 'Rest Api' })
+  response.render(pathJoin('./views/index.ejs'), {
+    title: 'Rest Api',
+    endpoints: routesTemplate
+  })
 }
 
 module.exports = routeRoot
